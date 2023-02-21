c2d03a22c707e50fdc61f491db4da8b9bf8178f8
Vitaly-Romanchuk <108594002+Vitaly-Romanchuk@users.noreply.github.com>
2023-02-22 01:48:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -144,6 +144,17 @@
     }
   }
 
+  	// Go to Top
+    $(window).on('scroll', function(){
+      var scrolled = $(window).scrollTop();
+      if (scrolled > 300) $('.go-top').addClass('active');
+      if (scrolled < 300) $('.go-top').removeClass('active');
+      });
+  // Click Event
+  $('.go-top').on('click', function() {
+  $("html, body").animate({ scrollTop: "0" },  500);
+  });
+
   // ------------------------ Навигация скролл: Navigation Scroll
   $(window).on("scroll", function () {
     var sticky = $(".sticky-menu"),
